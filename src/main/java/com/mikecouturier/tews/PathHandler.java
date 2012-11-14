e653485f383291366a530e6ac6184e4a44be67e7
Mike Gleason jr Couturier <mikegleasonjr@gmail.com>
2012-11-13 21:21:08 -0500

    added content-type, status code & headers support

--- src/main/java/com/mikecouturier/tews/PathHandler.java
+++ src/main/java/com/mikecouturier/tews/PathHandler.java
@@ -1,30 +1,46 @@
 package com.mikecouturier.tews;
 
 import org.mortbay.jetty.Request;
+import org.mortbay.jetty.Response;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.util.Map;
 
 public class PathHandler implements Servlet {
-    private String path;
+    private PathSpecification pathSpecification;
 
-    public PathHandler(String path) {
-        this.path = path;
+    public PathHandler(PathSpecification pathSpecification) {
+        this.pathSpecification = pathSpecification;
     }
 
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         Request request = (Request)servletRequest;
-
+        Response response = (Response)servletResponse;
         System.out.println(String.format("[TEWS] http://localhost:%d%s", request.getServerPort(), request.getRequestURI()));
 
         // Ugly hack, mapping the root catch all URLs on server
         // see "Default Mapping":
         // http://account.pacip.com/jetty/doc/PathMapping.html
-        if (path.equals("/") && !request.getRequestURI().equals("/")) {
+        if (pathSpecification.getPath().equals("/") && !request.getRequestURI().equals("/")) {
             request.setHandled(false);
             return;
         }
+
+        if (pathSpecification.getResponseSpecification().getBody() != null) {
+            servletResponse.getWriter().write(pathSpecification.getResponseSpecification().getBody());
+        }
+
+        if (pathSpecification.getResponseSpecification().getContentType() != null) {
+            servletResponse.setContentType(pathSpecification.getResponseSpecification().getContentType());
+        }
+
+        for (Map.Entry<String, String> header : pathSpecification.getResponseSpecification().getHeaders().entrySet()) {
+            response.addHeader(header.getKey(), header.getValue());
+        }
+
+        response.setStatus(pathSpecification.getResponseSpecification().getStatusCode());
     }
 
     @Override
--- src/test/java/com/mikecouturier/tews/TewsTest.java
+++ src/test/java/com/mikecouturier/tews/TewsTest.java
@@ -1,7 +1,6 @@
 package com.mikecouturier.tews;
 
 import org.junit.After;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.expect;
@@ -78,34 +77,91 @@ public class TewsTest {
     @Test
     public void aServerStatusCodeIs404ByDefault() throws Exception {
         server();
+
         expect().statusCode(404).when().get("/");
     }
 
     @Test
     public void servingAUrlReturnsCode200() throws Exception {
         serve("/").server();
+
         expect().statusCode(200).when().get("/");
     }
 
     @Test
     public void servingTheRootDoesNotServeAnythingElse() throws Exception {
         serve("/").server();
+
         expect().statusCode(404).when().get("/anything-else");
+    }
+
+    @Test
+    public void aUrlDefaultContentTypeIsTextPlain() throws Exception {
+        serve("/").server();
+
+        expect().contentType("text/plain").when().get("/");
     }
 
     @Test
     public void aServerCanServeMultipleUrls() throws Exception {
         serve("/url/1").serve("/index.html").server();
+
         expect().statusCode(200).when().get("/url/1");
         expect().statusCode(200).when().get("/index.html");
     }
 
     @Test
-    @Ignore
     public void aUrlCanServeData() throws Exception {
         String data = "data";
+
         serve("/url").responding().body(data).server();
+
         expect().body(equalTo(data)).when().get("/url");
+    }
+
+    @Test
+    public void aUrlCanServeACustomContentType() throws Exception {
+        String contentType = "application/json";
+
+        serve("/json").responding().contentType(contentType).server();
+
+        expect().contentType(contentType).when().get("/json");
+    }
+
+    @Test
+    public void aUrlCanServeACustomStatusCode() throws Exception {
+        int statusCode = 401;
+
+        serve("/unauthorized").responding().statusCode(statusCode).server();
+
+        expect().statusCode(statusCode).when().get("/unauthorized");
+    }
+
+    @Test
+    public void aUrlCanServeACustomHeader() throws Exception {
+        String headerName = "Cache-Control";
+        String headerValue = "no-cache";
+
+        serve("/uncached/page").responding().header(headerName, headerValue).server();
+
+        expect().header(headerName, headerValue).when().get("/uncached/page");
+    }
+
+    @Test
+    public void aUrlCanServeMultipleCustomHeaders() throws Exception {
+        String headerName1 = "Cache-Control";
+        String headerValue1 = "no-cache";
+        String headerName2 = "Authorization";
+        String headerValue2 = "Basic";
+
+        serve("/multiple-headers").responding()
+                .header(headerName1, headerValue1)
+                .header(headerName2, headerValue2)
+                .server();
+
+        expect().header(headerName1, headerValue1)
+                .header(headerName2, headerValue2)
+                .when().get("/multiple-headers");
     }
 
     @After
